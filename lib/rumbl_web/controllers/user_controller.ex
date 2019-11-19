41ca9205c526193cf1089056ab4ac2f85615063f
pawel.kwietniewski <kniarzyn@gmail.com>
2019-11-19 22:28:02 +0100

    Chapter 4 - done

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
 
   def index(conn, _) do
     users = Accounts.list_users()
@@ -8,7 +9,26 @@ defmodule RumblWeb.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-	user = Accounts.get_user(id)
+    user = Accounts.get_user(id)
     conn |> render("show.html", user: user)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> put_flash(:error, "Formularz zawiera błędy!")
+        |> render("new.html", changeset: changeset)
+    end
   end
 end
