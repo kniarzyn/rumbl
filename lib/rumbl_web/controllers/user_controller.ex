87cb806d48d673a651508e3bd0d47e83c89f54c5
pawel.kwietniewski <kniarzyn@gmail.com>
2019-11-22 23:02:59 +0100

    Make authenticate user fn more accessable

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,7 +3,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _) do
     users = Accounts.list_users()
@@ -32,17 +32,6 @@ defmodule RumblWeb.UserController do
         conn
         |> put_flash(:error, "Formularz zawiera błędy!")
         |> render("new.html", changeset: changeset)
-    end
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
     end
   end
 end
